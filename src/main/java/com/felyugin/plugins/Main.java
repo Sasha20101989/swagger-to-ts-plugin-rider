74a7445016bbd3f501a5ba8534639bfaab40dfc6
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2025-02-24 08:39:26 +0300

    Update Main.java

--- src/main/java/com/felyugin/plugins/Main.java
+++ src/main/java/com/felyugin/plugins/Main.java
@@ -8,27 +8,22 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        // Указываем URL Swagger и путь для сохранения TypeScript файла
         String swaggerUrl = ""; // Путь к Swagger
         String outputPath = "/Users/aleksander/Developer/Rubius/team-rubius-back/api-clients/TypeScript/ApiClient.ts"; // Путь для сохранения TypeScript файла
         String nswagPath = Objects.requireNonNull(Main.class.getResource("/NSwag/nswag")).getPath();
         try {
             deleteFileIfExists(outputPath);
-
-            // Логирование начала процесса
+            
             System.out.println("Starting process...");
-
-            // Проверка существования файла nswag
+            
             File nswagFile = new File(nswagPath);
             if (!nswagFile.exists()) {
                 System.out.println("NSwag executable not found at " + nswagPath);
                 return;
             }
-
-            // Логирование существования файла
+            
             System.out.println("NSwag executable found at: " + nswagPath);
-
-            // Строим команду для выполнения с путями, полученными из переменных
+            
             String[] nswagCommand = {
                     nswagPath,
                     "openapi2tsclient",
@@ -42,46 +37,36 @@ public class Main {
             };
 
             System.out.println("Running command: " + String.join(" ", nswagCommand));
-
-            // Запуск процесса
+            
             ProcessBuilder processBuilder = new ProcessBuilder(nswagCommand)
                     .redirectErrorStream(true);
             Process process = processBuilder.start();
-
-            // Логируем начало чтения потока
+            
             System.out.println("Reading process output...");
-
-            // Получаем вывод процесса по строкам
+            
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             StringBuilder output = new StringBuilder();
-
-            // Читаем вывод по строкам
+            
             while ((line = reader.readLine()) != null) {
                 output.append(line).append("\n");
-                // Логируем каждую строку
                 System.out.println("Process output: " + line);
             }
-
-            // Логируем полный вывод
+            
             System.out.println("Full process output:\n" + output.toString());
-
-            // Получаем код завершения
+            
             boolean processSuccess = process.waitFor(60, TimeUnit.SECONDS);
             if (processSuccess) {
                 replaceBaseUrl(outputPath);
                 addTsNoCheck(outputPath);
                 addImports(outputPath);
             } else {
-                // Логируем ошибку с кодом завершения
                 System.out.println("NSwag command failed with exit code: " + process.exitValue());
             }
         } catch (IOException e) {
-            // Логируем ошибку ввода/вывода
             System.out.println("An error occurred during the process: " + e.getMessage());
             e.printStackTrace();
         } catch (InterruptedException e) {
-            // Логируем прерывание процесса
             System.out.println("The process was interrupted: " + e.getMessage());
             e.printStackTrace();
         }
@@ -89,23 +74,17 @@ public class Main {
 
     public static void replaceBaseUrl(String outputPath) {
         try {
-            // Читаем весь файл в строку
             String content = new String(Files.readAllBytes(Paths.get(outputPath)));
-
-            // Найдем строки, которые начинаются с "this.baseUrl = baseUrl ?? " и заканчиваются на ";"
+            
             int index = content.indexOf("this.baseUrl = baseUrl ?? \"http");
             while (index != -1) {
-                // Ищем конец строки (если она заканчивается на ";")
                 int endIndex = content.indexOf(";", index);
                 if (endIndex != -1) {
-                    // Заменяем строку на нужную
                     content = content.substring(0, index) + "this.baseUrl = baseUrl ?? this.baseApiClient;" + content.substring(endIndex + 1);
                 }
-                // Ищем следующее вхождение
                 index = content.indexOf("this.baseUrl = baseUrl ?? \"http", endIndex);
             }
-
-            // Сохраняем изменения в файл
+            
             Files.write(Paths.get(outputPath), content.getBytes());
 
             System.out.println("BaseUrl replaced successfully.");
@@ -134,16 +113,12 @@ public class Main {
 
     public static void addTsNoCheck(String outputPath) {
         try {
-            // Читаем весь файл в строку
             String content = new String(Files.readAllBytes(Paths.get(outputPath)));
-
-            // Ищем место, куда нужно добавить строку
+            
             int index = content.indexOf("/* eslint-disable */");
             if (index != -1) {
-                // Добавляем строки сразу после /* eslint-disable */
                 content = content.substring(0, index + "/* eslint-disable */".length()) + "\n// @ts-nocheck" + content.substring(index + "/* eslint-disable */".length());
-
-                // Сохраняем изменения в файл
+                
                 Files.write(Paths.get(outputPath), content.getBytes());
 
                 System.out.println("Comments added successfully.");
@@ -158,12 +133,10 @@ public class Main {
 
     public static void addImports(String outputPath) {
         try {
-            // Читаем весь файл в строку
             String content = new String(Files.readAllBytes(Paths.get(outputPath)));
 
             int index = content.indexOf("import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, CancelToken } from 'axios';");
             if (index != -1) {
-                // Добавляем импорты сразу после комментариев
                 String imports = "\n\nimport { BaseApiClient } from './BaseApiClient';";
                 content = content.substring(0, index + "import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, CancelToken } from 'axios';".length()) + imports + content.substring(index + "import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, CancelToken } from 'axios';".length());
             } else {
